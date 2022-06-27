b15519689a5c393c64a1b7de44d09d9f0538b339
Dave Shawley <daveshawley@gmail.com>
2022-06-27 13:29:17 -0400

    Add missing branch coverage in tests

--- tests.py
+++ tests.py
@@ -23,6 +23,9 @@ class Handler(problemdetails.ErrorWriter, web.RequestHandler):
         raise_error = self.get_query_argument('raise_error', default=sentinel)
 
         if raise_error is not sentinel:
+            if raise_error == 'unhandled':
+                raise RuntimeError('unhandled exception')
+
             query_args = [
                 arg for arg in self.request.query_arguments.keys()
                 if arg not in ('raise_error', 'status')
@@ -122,13 +125,19 @@ class ErrorWriterTests(testing.AsyncHTTPTestCase):
         response = self.send_query()
         self.assertEqual(response.headers['Content-Type'], 'application/json')
 
+    def test_unhandled_exception(self):
+        response = self.send_query(raise_error='unhandled')
+        self.assertEqual(response.headers['Content-Type'],
+                         'application/problem+json')
+        self.assertEqual(500, response.code)
+
 
 class ProblemTests(ErrorWriterTests):
     def get_app(self):
         return Application()
 
     def send_query(self, **query):
-        query['raise_error'] = True
+        query.setdefault('raise_error', True)
         return self.fetch('/?{0}'.format(urlencode(query)))
 
     def test_that_custom_attributes_can_be_set(self):
