8cd49693fee99ce9170f51244972b31119400efb
Dave Shawley <daves@aweber.com>
2023-10-20 17:30:16 -0400

    Remove unnecessary boilerplate from __init__.py
    If you don't have tornado installed then this library isn't going to
    work very well.  Since we are deriving the version from the package
    metadata we don't have to worry about importing the package before the
    dependencies are installed.

--- problemdetails/__init__.py
+++ problemdetails/__init__.py
@@ -1,15 +1,11 @@
 from importlib import metadata
 
+from problemdetails.errors import Problem
+from problemdetails.handlers import ErrorWriter, type_link_map
+
 version = metadata.version('tornado-problem-details')
 version_info = [int(c) if c.isdigit() else c for c in version.split('.')]
 
-__all__ = ['version', 'version_info']
-
-try:
-    from problemdetails.errors import Problem
-    from problemdetails.handlers import ErrorWriter, type_link_map
-    __all__ = [
-        'ErrorWriter', 'Problem', 'type_link_map', 'version', 'version_info'
-    ]
-except ImportError:  # pragma: nocover
-    pass
+__all__ = [
+    'ErrorWriter', 'Problem', 'type_link_map', 'version', 'version_info'
+]
